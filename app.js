ce97dfa8c8abed68ccfc021f3ebfa58e984655d1
Gulhasanli1993 <gulshanhasanli641@gmail.com>
2025-05-12 15:09:58 +0000

    jsdom

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a = (a) => {
+            for (let i = 1; i <= a; i++) {
+                $('.col-md-12').append(`<h1>${i}</h1>`);
+            }
+        }
+        a($('input').val())
+    });
+
+
+    $('h2').click(function (e) {
+        e.preventDefault();
+        $('h2').html('hello');
+    });
 });
+
+
+
