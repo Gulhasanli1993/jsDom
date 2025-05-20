5bad08e0e1dd2215dcac67e2417b21b21b44a3eb
Gulhasanli1993 <gulshanhasanli641@gmail.com>
2025-05-20 20:25:10 +0000

    jstask

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a = (a) => {
+            for (let i = 1; i <= a; i++) {
+                $('.col-md-12').append(`<h2>${i}</h2>`);
+            }
+        }
+        a($('input').val())
+    });
+
+
+    $('h2').click(function (e) {
+        e.preventDefault();
+        $('h2').html('hello');
+    });
 });
