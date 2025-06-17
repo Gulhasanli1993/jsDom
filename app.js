244f9035dce67a0ded0ab3cb0e6770fcbcd1fa77
Gulhasanli1993 <gulshanhasanli641@gmail.com>
2025-06-17 19:10:59 +0000

    document

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 "use strict";
 $(document).ready(function () {
-
     let input = document.querySelector("input[type='number']");
     let addBtn = document.querySelector(".btn h3");
-    let section = document.getElementById("jstask");
+    let section = document.getElementById("js.task");
     addBtn.addEventListener("click", function () {
-        let Number = Number(input.value);
+        let eded = Number(input.value);
         section.innerHTML = " ";
-
         for (let i = 1; i <= 5; i++) {
             let p = document.createElement("p");
             p.textContent = i;
