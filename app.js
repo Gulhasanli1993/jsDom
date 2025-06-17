b441bc21d18a23ca6c37975ed0ba74fd92135362
Gulhasanli1993 <gulshanhasanli641@gmail.com>
2025-06-17 15:53:32 +0000

    addBtn

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 "use strict";
 $(document).ready(function () {
 
-    
+    let input = document.querySelector("input[type='number']");
+    let addBtn = document.querySelector(".btn h3");
+    let section = document.getElementById("jstask");
+    addBtn.addEventListener("click", function () {
+        let Number = Number(input.value);
+        section.innerHTML = " ";
+
+        for (let i = 1; i <= 5; i++) {
+            let p = document.createElement("p");
+            p.textContent = i;
+            section.appendChild(p);
+        }
+    })
 });
