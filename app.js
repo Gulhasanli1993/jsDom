f675e79b5107da67b3da74f8163dabe32824849f
Gulhasanli1993 <gulshanhasanli641@gmail.com>
2025-07-09 11:24:56 +0000

    function

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 "use strict";
-$(document).$(document).ready(function () {
-    let input = document.querySelector("input[type='number']");
-    let addBtn = document.querySelector(".btn h3");
-    let section = document.getElementById("js.task");
-    addBtn.addEventListener("click", function () {
-        let added = Number(input.value);
-        section.innerHTML = " ";
-        for (let i = 1; i <= 5; i++) {
-            let p = document.createElement("p");
-            p.textContent = i;
-            section.appendChild(p);
-        }
-    })
+$(document).ready(function () {
+    let input = document.getElementById("numberInput");
+    let btn = document.getElementsByClassName("btn");
+    let textbox = document.getElementsByClassName("textbox");
+    btn.addEventListener("click", function () {
+        
+    });
 });
 
