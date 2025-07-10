cafb535855c58aaa6f3ad34fab4483b5cd8e0b83
Gulhasanli1993 <gulshanhasanli641@gmail.com>
2025-07-10 13:12:49 +0000

    box

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ $(document).ready(function () {
     let btn = document.getElementsByClassName("btn");
     let textbox = document.getElementsByClassName("textbox");
     btn.addEventListener("click", function () {
-        
+
+        let deyer = parseInt(input.value);
+        let text = "";
+        for (let i = 0; i < deyer; i++) {
+            text += deyer;
+        }
+        netice.innerText = text;
     });
 });
 
